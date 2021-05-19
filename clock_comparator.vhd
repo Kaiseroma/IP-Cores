a20852dd2d5b8afee328f5ca9609985d280c77dc
EmbeddedKaiser <60993883+Kaiseroma@users.noreply.github.com>
2021-05-19 12:31:21 +0300

    Update clock_comparator.vhd

--- clock_comparator.vhd
+++ clock_comparator.vhd
@@ -1,3 +1,13 @@
+-----------------------------------------------------------------------------------
+-- Module: PRS
+-- Created on: 2021-03-22 14:31:22
+-- Target language: VHDL
+-- Author: keizerr
+-----------------------------------------------------------------------------------
+-- Description: This is a simple module for clock frequencies comparison. If the clk frequency is greater than opora frequency, the output signal is going
+-- to be 0, else 1
+-----------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -6,8 +16,8 @@ entity clkcmp is
 	port
 	(
 	clk		: in std_logic;
-	opora   : in std_logic;
-	reset	: in std_logic;
+	opora   	: in std_logic;
+	reset		: in std_logic;
 	q		: out std_logic
 	);
 end entity;
@@ -21,23 +31,19 @@ begin
 	begin
 		if (rising_edge(clk)) then
 			if reset = '1' then
-				-- сбрасываем счетчик в 0
 				cnt1 := 0;
 			else
-				-- инкрементируем счетчик
 				cnt1 := cnt1 + 1;
 			end if;
 		end if;
 		if (rising_edge(opora)) then
 			if reset = '1' then
-				-- сбрасываем счетчик в 0
 				cnt2 := 0;
 			else
-				-- инкрементируем счетчик
 				cnt2 := cnt2 + 1;
 			end if;
 		end if;
-		-- устанавливаем текущее значение на вывод
+		-- making a comparison
 		if cnt1 > cnt2 then
 			q <='0';
 		else q <='1';
